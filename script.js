0fb2966fd2b435cfbf563c07b2b6f69bff1fe33f
haneen <haneenhariry8@gmail.com>
2024-05-05 16:58:08 +0300

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+function change (){
+    let list = document.getElementById("listChange");
+    let list2 = document.getElementById("listChange1");
+    list.style.display="none"
+    list2.style.display= "block";
+}
+function change2 (){
+    let list2 = document.getElementById("listChange1");
+    let list = document.getElementById("listChange");
+    list.style.display= "block";
+    list2.style.display= "none";
+}
